23f2604cdeb859e02e11ed46db3e5c9c61a094b8
lee <sipossa@naver.com>
2022-09-21 13:50:32 +0900

    20220921

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Wrapper from './pages/Wrapper';
+import Header from './pages/Header';
+import Main from './pages/Main';
+import Sub01 from './pages/Sub01';
+import Sub02 from './pages/Sub02';
+import Sub03 from './pages/Sub03';
+import Sub04 from './pages/Sub04';
+import Board from './pages/Board';
+import Footer from './pages/Footer';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
+import './css/basic.scss';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/sub01' element={<Sub01 />} />
+        <Route path='/sub02' element={<Sub02 />} />
+        <Route path='/sub03' element={<Sub03 />} />
+        <Route path='/sub04' element={<Sub04 />} />
+        <Route path='/board' element={<Board />} />
+      </Routes>
+      <Footer />
+    </Wrapper>
+  )
 }
 
 export default App;
