95458f0283b72e88741eed2826fe9d7999b87db7
lee <sipossa@naver.com>
2022-09-22 17:25:09 +0900

    20220922

--- src/pages/Sub04.js
+++ src/pages/Sub04.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+const Sub04 = ({ user }) => {
 
-const Sub04 = () => {
     return (
-        <div>Sub04</div>
+        <div className='inner'>
+            {
+                !user
+                    ? <h1>"로딩중입니다. ... "</h1>
+                    : <ul>
+                        {
+                            user.map(u => {
+                                return (
+                                    <li key={u.id}>
+                                        <Link to={'/detail/' + u.id}>
+                                            <img src={u.image_link} alt="" />
+                                            <div className="name">
+                                                {u.name}
+                                            </div>
+                                            <div className="price">
+                                                {u.price}
+                                            </div>
+
+                                        </Link>
+
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+            }
+        </div>
     )
 }
 
-export default Sub04
+export default Sub04;
