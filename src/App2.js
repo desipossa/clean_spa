456ff5373ac79fe4c51671303664a54873fe220a
lee <sipossa@naver.com>
2022-09-23 17:19:19 +0900

    20220923

--- src/App2.js
+++ src/App2.js
@@ -8,9 +8,9 @@ import Sub03 from './pages/Sub03';
 import Sub04 from './pages/Sub04';
 import Board from './pages/Board';
 import Footer from './pages/Footer';
-import Detail from './pages/Detail';
-import MainContentData from './pages/MainContentData'
 import { Route, Routes } from 'react-router-dom';
+import MainContentData from './pages/MainContentData'
+import Detail from './pages/Detail';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './css/basic.scss';
@@ -20,23 +20,22 @@ import './css/basic.scss';
 const url = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
 const App = () => {
   const [user, setUser] = useState();
-  const getData = async () => {
-    const res = await axios.get(url);
-    setUser(res.data);
-  }
+
   useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get(url);
+      setUser(res.data);
+    }
     getData();
   }, [])
   return (
     <Wrapper>
       <Header />
-      {console.log(user)}
       <Routes>
         <Route path='/' element={<MainContentData user={user} />} />
         <Route path='/sub04' element={<Sub04 user={user} />} />
         <Route path='/detail/:id' element={<Detail user={user} />} />
       </Routes>
-      <Footer />
     </Wrapper>
   )
 }
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,22 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom'
 
 const Detail = ({ user }) => {
     const { id } = useParams();
-    // const [muser, setUser] = useState()
-    // const xuser = user?.find(i => i.id === Number(id))
-    // useEffect(() => {
-
-    //     console.log(user, muser, xuser)
-    // }, [])
-    console.log(user)
-    const muser = user?.find(i => i.id === Number(id))
+    const muser = user.find(i => Number(id) == i.id)
     return (
         <>
             {
-                <div>
-                    <img src={muser?.image_link} alt="" />
-                </div>
+                !muser ? <div>LOADING ...</div>
+                    :
+                    <div>
+                        <img src={muser.image_link} alt="" />
+                    </div>
             }
         </>
 
